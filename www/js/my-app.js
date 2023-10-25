b0a7e92ff4f8191350d903de875638add2fdd9a4
Lucas <lucas.gonzalez01@outlook.com>
2023-10-25 17:25:24 -0300

    segundo commit, agregando firebase

--- www/js/my-app.js
+++ www/js/my-app.js
@@ -14,7 +14,7 @@ var app = new Framework7({
     },
     // Add default routes
     routes: [
-        { path: '/about/',            url: 'about.html',    },
+        { path: '/login/',            url: 'login.html',    },
         { path: '/confirmacion/',     url: 'confirmacion.html',    },
         { path: '/index/',            url: 'index.html',    },
         { path: '/info/',             url: 'info.html',    },
@@ -44,18 +44,47 @@ $$(document).on('page:init', '.page[data-name="about"]', function (e) {
 
 $$(document).on('page:init', '.page[data-name="index"]', function (e) {
 $$("#btnRegistro").on("click", fnRegistro);
-  
-})
+
 
 $$(document).on('page:init', '.page[data-name="registro"]', function (e) {
   $$("#btnFinReg").on("click", fnFinRegistro);
-  
 })
 
 $$(document).on('page:init', '.page[data-name="confirmacion"]', function (e) {
 $$("#confNombre").text(nombre)
 $$("#confEmail").text(email)
   
+   // onSuccess Callback
+    // This method accepts a Position object, which contains the
+    // current GPS coordinates
+    //
+    var fnLeeOkelGPS = function(position) {
+      latitud = + position.coords.latitude
+      longitud = + position.coords.longitude
+
+      $$("#confLatitud").text(latitud)
+      $$("#confLongitud").text(longitud)
+      
+      /* alert('Latitude: '          + position.coords.latitude          + '\n' +
+            'Longitude: '         + position.coords.longitude         + '\n' +
+            'Altitude: '          + position.coords.altitude          + '\n' +
+            'Accuracy: '          + position.coords.accuracy          + '\n' +
+            'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+            'Heading: '           + position.coords.heading           + '\n' +
+            'Speed: '             + position.coords.speed             + '\n' +
+            'Timestamp: '         + position.timestamp                + '\n'); */
+  };
+
+  // onError Callback receives a PositionError object
+  //
+  function onErrorGPS(error) {
+      alert('code: '    + error.code    + '\n' +
+            'message: ' + error.message + '\n');
+  }
+
+  navigator.geolocation.getCurrentPosition(fnLeeOkelGPS, onErrorGPS);
+  
+})
 })
 
 $$(document).on('page:init', '.page[data-name="info"]', function (e) {
@@ -65,14 +94,39 @@ $$(document).on('page:init', '.page[data-name="info"]', function (e) {
 
 
 /* mis funciones */
-var email, clave, nombre, apellido;
+var email, clave, nombre, apellido, latitud, longitud;
 
 function fnRegistro() {
 email = $$("#indexEmail").val();
 clave = $$("#indexClave").val();
 
 if (email!="" && clave!="") {
-  mainView.router.navigate("/registro/")
+    // cada un@ pone su magia para recuperar el mail y la clave de un form...
+  
+    firebase.auth().createUserWithEmailAndPassword(email, clave)
+      .then((userCredential) => {
+        // Signed in
+        var user = userCredential.user;
+        console.log("Bienvenid@!!! " + email);
+        // ...
+        mainView.router.navigate('/registro/');
+      })
+      .catch((error) => {
+        var errorCode = error.code;
+        var errorMessage = error.message;
+
+        console.error(errorCode);
+        console.error(errorMessage);
+
+        if (errorCode == "auth/email-already-in-use") {
+            console.error("el mail ya esta usado");
+        }
+
+        // ..
+      });
+
+  
+  // mainView.router.navigate("/registro/")
 }
 }
 
